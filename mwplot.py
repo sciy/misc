6f4c5e5a837370dac342b0b049406e2208344e24
mophi <mogeiwang@gmail.com>
2014-07-01 18:04:43 +0800

    Create mwplot.py

---
+++ mwplot.py
@@ -0,0 +1,15 @@
+figure()
+subplot(2,1,1)
+plot(...) #plot loglog semilogx
+legend([..., ...], ['...', '...'])
+xlim(0, ...)# set axis limits
+ylim(0, ...)
+subplot(2,1,2)
+plot(...)
+legend([..., ...], ['...', '...'])
+xlim(0, ...)
+ylim(0, ...)
+xlabel('...')# make axis labels
+ylabel('...')
+title('Title $%d$ ...'%i)# give plot a title
+savefig('...%05d.eps'%i)
