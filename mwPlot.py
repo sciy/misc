2cc6c3b8724cf55d67d1939d95ce5f6b8fb25361
mophi <mogeiwang@gmail.com>
2014-07-07 13:49:32 +0800

    Update mwPlot.py

--- mwPlot.py
+++ mwPlot.py
@@ -2,16 +2,13 @@
 # https://github.com/olgabot/prettyplotlib/tree/master/tests
 
 figure()
-subplot(2,1,1)
+subplot(1,1,1)
 plot(...) #plot loglog semilogx; hist; semilogy; scatter ...
 legend([..., ...], ['...', '...'])
 xlim(0, ...)# set axis limits
 ylim(0, ...)
-subplot(2,1,2)
-plot(...)
-legend([..., ...], ['...', '...'])
-xlim(0, ...)
-ylim(0, ...)
+xticks([...])
+yticks([...])
 xlabel('...')# make axis labels
 ylabel('...')
 title('Title $%d$ ...'%i)# give plot a title
