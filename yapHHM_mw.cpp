8d1ef5b54b052ac8990da5fd542d361dd15e9de1
mophi <mogeiwang@gmail.com>
2015-02-06 23:14:23 +0800

    Update yapHHM_mw.cpp

--- yapHHM_mw.cpp
+++ yapHHM_mw.cpp
@@ -1,14 +1,14 @@
 // What is it? //// GSL (ODE), gnuplot, fileIO... ////////////////////////////////////////////////////////{{{
-// yapHHM ver 1.0
+// yapHHM ver 2.0
 // Yet Another Program solves the Hodgkin-Huxley Model (HHM),
 //     which describes how action potentials are initiated and propagated.
 // HHM is a continuous time mathematical model includes a set of nonlinear differential equations (ODE):
 //     dydt[0] = 0; // y[0] is the input (injection current) I. No evolution. Written as an equation for convenience.
 //     dydt[1] = y[0]/C_m -g_Na*y[2]*y[2]*y[2]*y[4]*(y[1]-E_Na)/C_m -g_K*y[3]*y[3]*y[3]*y[3]*(y[1]-E_K)/C_m -g_L*(y[1]-E_L)/C_m;
-//     dydt[2] = (2.5-0.1*y[1])*(1-y[2])/(exp(2.5-0.1*y[1])-1) -4*y[2]*exp(-y[1]/18); // y[2] is m
-//     dydt[3] = (0.1-0.01*y[1])*(1-y[3])/(exp(1-0.1*y[1])-1) -0.125*y[3]*exp(-y[1]/80); // y[3] is n
-//     dydt[4] = 0.07*(1-y[4])*exp(-y[1]/20) -y[4]/(exp(3-0.1*y[1])+1); // y[4] is h
-// Parameters:     double E_Na = 115, E_K = -12, E_L = 10.6, g_Na = 120, g_K = 36, g_L = 0.3, C_m = 1;
+//     dydt[2] = (2.5-0.1*y[1])    *(1-y[2])/(exp(2.5-0.1*y[1])-1) -4    *y[2]/exp(y[1]/18); // y[2] is m
+//     dydt[3] = (0.1-0.01*y[1])   *(1-y[3])/(exp(1-0.1*y[1])-1)   -0.125*y[3]/exp(y[1]/80); // y[3] is n
+//     dydt[4] = 0.07              *(1-y[4])/exp(y[1]/20)          -1    *y[4]/(exp(3-0.1*y[1])+1); // y[4] is h
+// Parameters:     E_Na = 115, E_K = -12, E_L = 10.6, g_Na = 120, g_K = 36, g_L = 0.3, C_m = 1; //Mode 0
 // Initialization: y[1] = 0, y[2] = 0, y[3] = 0, y[4] = 0;
 // The above equations and parameters are taken from the book
 //     Spiking Neuron Models: Single Neurons, Populations, Plasticity. W Gerstner, W M Kistler. Cambridge University Press, 2002
@@ -19,11 +19,10 @@
 //     http://www.gnuplot.info/help.html
 //     http://www.stahlke.org/dan/gnuplot-iostream/
 //
-// g++ -static yapHHM_mw.cpp -lm -lgsl -lgslcblas  -lboost_system -lboost_filesystem -lboost_iostreams  -o yapHHM1
+// g++ -static yapHHM_mw.cpp -lm -lgsl -lgslcblas  -lboost_system -lboost_filesystem -lboost_iostreams  -o yapHHM2
 //
 // Mogei Wang (mogeiwang@gmail.com) on xUbuntu 14.04
 // 2015-2-2 in Dalian, China
-//
 //////////////////////////////////////////////////////////////////////////////////////////////////////////}}}
 
 #include <string>//{{{
@@ -38,6 +37,7 @@
 using namespace std;
 
 const int SystemDimension = 5;
+const double Mode = 0; // Mode 0: E_x use HH 1952 parameters; Mode 1: E_x-65
 Gnuplot plotter;
 ifstream fin;
 ofstream fout;//}}}
@@ -54,9 +54,9 @@ int func (double t, const double y[], double dydt[], void *params)// Defining fu
 
     dydt[0] = 0; // y[0] is the input current. No evolution. Written as an equation for convenience.
     dydt[1] = y[0]/C_m -g_Na*y[2]*y[2]*y[2]*y[4]*(y[1]-E_Na)/C_m -g_K*y[3]*y[3]*y[3]*y[3]*(y[1]-E_K)/C_m -g_L*(y[1]-E_L)/C_m;//u
-    dydt[2] = (2.5-0.1*y[1])*(1-y[2])/(exp(2.5-0.1*y[1])-1) -4*y[2]*exp(-y[1]/18); // y[2] is m
-    dydt[3] = (0.1-0.01*y[1])*(1-y[3])/(exp(1-0.1*y[1])-1) -0.125*y[3]*exp(-y[1]/80); // y[3] is n
-    dydt[4] = 0.07*(1-y[4])*exp(-y[1]/20) -y[4]/(exp(3-0.1*y[1])+1); // y[4] is h
+    dydt[2] = (2.5-0.1*y[1])    *(1-y[2])/(exp(2.5-0.1*y[1])-1) -4    *y[2]/exp(y[1]/18); // y[2] is m
+    dydt[3] = (0.1-0.01*y[1])   *(1-y[3])/(exp(1-0.1*y[1])-1)   -0.125*y[3]/exp(y[1]/80); // y[3] is n
+    dydt[4] = 0.07              *(1-y[4])/exp(y[1]/20)          -1    *y[4]/(exp(3-0.1*y[1])+1); // y[4] is h
     return GSL_SUCCESS;
 }//}}}
 
@@ -85,50 +85,31 @@ int main (void)
     {
         cout << "out file not opend!" << endl;
         return -1;
-    }
-//
-//     fin.open ("yapHHM_InjectionCurrent.txt",ios::in); //|ios::binary);// NOT Used. Input is written as inner codes.
-//     if (! fin.is_open() )
-//     {
-//         cout << "in file not opend!" << endl;
-//         return -1;
-//     }//}}}
+    }//}}}
 
-// //  Notes for file IO:{{{
-// //     fout << y[0] << "; " << C_m << "; " << 12.345 << "; " << endl; // output to file.
-// //
-// //     string  s;
-// //     while ( fin  >>  s ) // input from file word by word.
-// //     {
-// //         cout  <<   " Read from file:  "   <<  s  <<  endl;
-// //     }
-// //     while ( getline(fin,s) ) // input from file line by line.
-// //     {
-// //         cout  <<   " Read from file:  "   <<  s  <<  endl;
-// //     }//}}}
-
-    double C_m = 1, E_Na = 115, E_K = -12, E_L = 10.6, g_Na = 120, g_K = 36, g_L = 0.3;//{{{
+    double C_m = 1.0, E_Na = 115 - Mode * 65, E_K = -12 - Mode * 65, E_L = 10.6 - Mode * 65, g_Na = 120, g_K = 36, g_L = 0.3;//{{{
     double params[] = {C_m, g_Na, E_Na, g_K, E_K, g_L, E_L};
     gsl_odeiv2_system sys = {func, jac, SystemDimension, params}; // build the sys for evolution.
     gsl_odeiv2_driver * d = gsl_odeiv2_driver_alloc_y_new (&sys, gsl_odeiv2_step_rk8pd, 1e-6, 1e-6, 0.0);
 //  gsl_odeiv2_driver * d = gsl_odeiv2_driver_alloc_y_new (&sys, gsl_odeiv2_step_rk4,   1e-3, 1e-8, 1e-8);
 
-    double t = 0.0, t1 = 20.0, ts = 200.0; // runtime from 0 to t1; t is the current time; ts is how many time steps
+    double t = 0.0, t1 = 100.0, tsfactor = 10.0, ts = tsfactor * t1; // t: current time, from 0 to t1; ts: how many time steps
     double y[SystemDimension] = { 0.0, 0.0, 0.0, 0.0, 0.0 };
-    cout << "#t  " << "input   " << "y[1]   " << "y[2]   " << "y[3]   " << "y[4]" << endl;
-    fout << "#t  " << "input   " << "y[1]   " << "y[2]   " << "y[3]   " << "y[4]" << endl;
-    cout << t << "  " << y[0] << "  " << y[1] << "  " << y[2] << "  " << y[3] << "  " << y[4] << endl;
-    fout << t << "  " << y[0] << "  " << y[1] << "  " << y[2] << "  " << y[3] << "  " << y[4] << endl;//}}}
+    cout << "#t  " << "input   " << "y[1]   " << "y[2]   " << "y[3]   " << "y[4]   " << "Na+   " << "K+   " << endl;
+    fout << "#t  " << "input   " << "y[1]   " << "y[2]   " << "y[3]   " << "y[4]   " << "Na+   " << "K+   " << endl;
+    cout << t << "  " << y[0] << "  " << y[1] << "  " << y[2] << "  " << y[3] << "  " << y[4] <<  "  " << g_Na*y[2]*y[2]*y[2]*y[4] << "  " << g_K*y[3]*y[3]*y[3]*y[3] << endl;
+    fout << t << "  " << y[0] << "  " << y[1] << "  " << y[2] << "  " << y[3] << "  " << y[4] <<  "  " << g_Na*y[2]*y[2]*y[2]*y[4] << "  " << g_K*y[3]*y[3]*y[3]*y[3] << endl;
+    //}}}
 
     for (int i = 1; i <= round(ts); i++)// i is the number of current time step{{{
     {
-        y[0] = 0.0; // The input current sets here !!! vvv !!!
-        if (i == 20) y[0] = 0.00; // an injection current is input...
+        double ti = i / tsfactor;
+        if (i == (30 * tsfactor    )) y[0] = 7.3; // i == x*tsfactor: y[0]=y: turn the input voltage to y at time x
+        if (i == (31 * tsfactor    )) y[0] = 0  ; // i == zzz       : y[0]=0: turn the input voltage to 0 at time step zzz
 
-        double ti = i*t1/ts; int status = gsl_odeiv2_driver_apply (d, &t, ti, y); // The step-size is automatically adjusted.
-// //      gsl_odeiv2_driver_apply ( d,  t, t1, y[]) evolves the driver system d from t to t1.
-
+        int status = gsl_odeiv2_driver_apply (d, &t, ti, y); // The step-size is automatically adjusted.
 //         int status = gsl_odeiv2_driver_apply_fixed_step (d, &t, 1e-3, 1000, y); // Fixed step.
+// //      gsl_odeiv2_driver_apply ( d,  t, t1, y[]) evolves the driver system d from t to t1.
 // //      gsl_odeiv2_driver_apply_fixed_step ( d,  t, h, n, y[]) evolves the driver system d from t with n steps of size h.
 
         if (status != GSL_SUCCESS)
@@ -137,40 +118,39 @@ int main (void)
             break;
         }
 
-        cout << t << "  " << y[0] << "  " << y[1] << "  " << y[2] << "  " << y[3] << "  " << y[4] << endl;
-        fout << t << "  " << y[0] << "  " << y[1] << "  " << y[2] << "  " << y[3] << "  " << y[4] << endl;
+        cout << t << "  " << y[0] << "  " << y[1] << "  " << y[2] << "  " << y[3] << "  " << y[4] <<  "  " << g_Na*y[2]*y[2]*y[2]*y[4] << "  " << g_K*y[3]*y[3]*y[3]*y[3] << endl;
+        fout << t << "  " << y[0] << "  " << y[1] << "  " << y[2] << "  " << y[3] << "  " << y[4] <<  "  " << g_Na*y[2]*y[2]*y[2]*y[4] << "  " << g_K*y[3]*y[3]*y[3]*y[3] << endl;
     }//}}}
 
     plotter << "set terminal post enh" << endl;//enhanced PostScript{{{ {{{
     plotter << "set output \"yapHHM_MembraneVoltage.eps\"" << endl;
-    plotter << "set xrange [0:20]" << endl;
-    plotter << "set xtics 0,5,100" << endl;
+    plotter << "set xrange [0:" << t1 << "]" << endl;
+    plotter << "set xtics 0,5," << t1 << endl;
     plotter << "unset key" << endl;
     plotter << "set title \"Hodgkin-Huxley Model\"" << endl;
     plotter << "set xlabel \"Time (mS)\"" << endl;//}}}
 
-    plotter << "set ylabel \"Input Current ({/Symbol \\m}A/cm^{2})\"" << endl;//{{{
-    plotter << "set yrange [-0.5:0.5]" << endl;
-    plotter << "set ytics -2,0.1,10" << endl;
-    plotter << "plot \"yapHHM_MembraneVoltage.txt\" using 1:2 with lp lc 1  lw 1  pt 1  ps 1 title \"I\"" << endl;
+    plotter << "set ylabel \"Input Current per unit area ({/Symbol \\m}A/cm^{2})\"" << endl;//{{{
+    plotter << "set yrange [" << -2 << ":" << 12 << "]" << endl;
+    plotter << "set ytics -100,1,100" << endl;
+    plotter << "plot \"yapHHM_MembraneVoltage.txt\" using 1:2 with line lc 1  lw 2  pt 1  ps 1 title \"I\"" << endl;
 
     plotter << "set ylabel \"Membrane Voltage (mV)\"" << endl;
-    plotter << "set yrange [-15:90]" << endl;
-    plotter << "set ytics -20,5,100" << endl;
-    plotter << "plot \"yapHHM_MembraneVoltage.txt\" using 1:3 with lp lc 2  lw 1  pt 2  ps 1 title \"u\"" << endl;
+    plotter << "set yrange [" << -20 - Mode * 65 << ":" << 110 - Mode * 65 << "]" << endl;
+    plotter << "set ytics -200,5,200" << endl;
+    plotter << "plot \"yapHHM_MembraneVoltage.txt\" using 1:3 with line lc 2  lw 2  pt 2  ps 1 title \"u\"" << endl;
 
     plotter << "set key" << endl;
-    plotter << "set ylabel \"Voltage of Na^{+} (m), K^{+} (m), and Leak (h) Channel (mV)\"" << endl;
-    plotter << "set yrange [-0.3:1.2]" << endl;
-    plotter << "set ytics -2,0.1,2" << endl;
-    plotter << "plot \"yapHHM_MembraneVoltage.txt\" u 1:4 w lp lc 4  lw 1  pt 4  ps 1 t \"m\", \"yapHHM_MembraneVoltage.txt\" u 1:5 w lp lc 6  lw 1  pt 6  ps 1 t \"n\", \"yapHHM_MembraneVoltage.txt\" u 1:6 w lp lc 8  lw 1  pt 8  ps 1 t \"h\"" << endl;//}}}
+    plotter << "set ylabel \"Conductance of Na^{+} and K^{+} Channel ({/Symbol \\W}^{-1})\"" << endl;
+    plotter << "set yrange [" << -0.2 << ":" << 1.2 << "]" << endl;
+    plotter << "set ytics -100,0.1,100" << endl;
+    plotter << "plot \"yapHHM_MembraneVoltage.txt\" u 1:5 w l lc 7  lw 2  pt 7  ps 1 t \"Na^{+}\", \"yapHHM_MembraneVoltage.txt\" u 1:6 w l lc 8  lw 2  pt 8  ps 1 t \"K^{+}\"" << endl;//}}}
 
     plotter << "set output" << endl;//{{{
     plotter << "set term wxt" << endl;
     plotter << endl;//}}} }}}
 
-//     fin.close();//{{{
-    fout.close();
+    fout.close();//{{{
     gsl_odeiv2_driver_free (d);
     return 0;//}}}
 
