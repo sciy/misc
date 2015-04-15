990b1f77804c4af0342f12e800b7e40268876951
Mogei Wang <mogeiwang@gmail.com>
2015-04-15 10:29:09 +0800

    Update openGL-dynCurve.cc

--- openGL-dynCurve.cc
+++ openGL-dynCurve.cc
@@ -1,4 +1,4 @@
-// plot a moving sin curve with OpenGL.
+// plot a moving sin curve controled by keyboard with OpenGL.
 // gcc x.cc -o x -lGL -lGLU -lglut
 // By M.W.
 
@@ -9,8 +9,8 @@
 #include <GL/freeglut.h> // freeglut.h for all
 using namespace std;
 
-const float freq = 1;// plot sin(freq*x+delta), where delta is changing.
-float delta = 0, deltaStep = 0.01;
+float freq = 1, freqStep = 0.3, delta = 0, deltaStep = 0.3; // plot sin(freq*x+delta)
+int KeyboardState = 0; // state=0: change delta, 1 for freq
 
 void myDisplay(void)
 {
@@ -26,8 +26,49 @@ void myDisplay(void)
 
 void myAnimate(void) // update delta and redraw
 {
-		delta += deltaStep;
 		glutPostRedisplay();
+}
+
+void myKeyboard(unsigned char key,int x, int y)
+{
+        if (key == 'd' || key == 'D')
+        {
+            KeyboardState = 0;
+            cout << "switch to the delta change mode" << endl;
+        }
+        else if (key == 'f' || key == 'F')
+        {
+            KeyboardState = 1;
+            cout << "switch to the freq change mode" << endl;
+        }
+        else if (key == 27)
+        {
+            cout << "user terminated" << endl;
+            exit(0);
+        }
+        else
+        {
+            if (KeyboardState == 0 && key == '=')
+            {
+                delta += deltaStep;
+                cout << "increasing delta" << endl;
+            }
+            else if (KeyboardState == 0 && key == '-')
+            {
+                delta -= deltaStep;
+                cout << "decreasing delta" << endl;
+            }
+            else if (KeyboardState == 1 && key == '=')
+            {
+                freq += freqStep;
+                cout << "increasing freq" << endl;
+            }
+            else if (KeyboardState == 1 && key == '-')
+            {
+                freq -= freqStep;
+                cout << "decreasing freq" << endl;
+            }
+        }
 }
 
 int main(int argc, char ** argv)
@@ -36,9 +77,11 @@ int main(int argc, char ** argv)
 		glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB);
 
 		glutCreateWindow("hello, dynamic curve"); // create the window frame
-		glutDisplayFunc(myDisplay);// register the display function
-		glutIdleFunc(myAnimate);//register the update function
+		glutDisplayFunc(myDisplay); // register the display function
+		glutIdleFunc(myAnimate); //register the update function
+        glutKeyboardFunc(myKeyboard);
 
+        cout << "Plot a dynamic sin curve.\nPress D/F to switch parameters.\nPress -/= to decrease/increase them.\nPress Esc to quit." << endl;
 		glutMainLoop();
 		return(0);
 }
