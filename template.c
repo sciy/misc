5018b407a8c2bb0b14c5ace04fb8efe391025064
Mogei Wang <mogeiwang@gmail.com>
2015-06-10 23:25:56 +0800

    Update template.c

--- template.c
+++ template.c
@@ -14,8 +14,7 @@
 //·
 //==============================================/*}}}*/
 
-// --- includes and macros {{{
-#include <assert.h>/*{{{*/ //checked
+#include <assert.h>//{{{ //checked
 #include <ctype.h>
 #include <errno.h>
 #include <float.h>
@@ -30,13 +29,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <unistd.h> // kind of unix.h :-)
-#include <GL/freeglut.h>
-#include <sys/stat.h>
+#include <unistd.h> // unix.h :-)
+#include <GL/freeglut.h> // openGL
+#include <sys/stat.h> // unix files
 #include <sys/types.h>
-#include <gsl/gsl_errno.h>
-#include <gsl/gsl_matrix.h>
-#include <gsl/gsl_odeiv2.h>
 
 #define BEGIN {
 #define END }
@@ -44,44 +40,47 @@
 #define boolean integer
 #define real double
 #define elif else if
-#define true (1==1)   // gcc: 1=true, 0=false
-#define false (!true)
+#define true ((integer)(1==1))   // gcc: 1=true, 0=false
+#define false ((integer)(!true))
+#define EQ(f1, f2)          ( fabs((real)(f1)-(real)(f2))<(1e-13))
+
 #define max(a,b)  ( ((a)>=(b)) ? (a):(b) )
 #define min(a,b)  ( ((a)<=(b)) ? (a):(b) )
 #define IdentifierLiteral(T) #T // return "T"
 #define IdentifierJoint(T,x) T##x // return Tx
-
-#define  print  printf
-#define fprint fprintf
-#define pprint fprintf
-#define sprint sprintf
-#define  println()     print("\n")
-#define fprintln(f)   fprint(f, "\n")
-#define pprintln(p)   pprint(p, "\n")
-#define sprintln(s)   sprint(s, "\n")
-#define  printTab()    print("\t")
-#define fprintTab(f)  fprint(f, "\t")
-#define pprintTab(p)  pprint(p, "\t")
-#define sprintTab(s)  sprint(s, "\t")
-#define print_integer(pp)   print("\t%ld", pp)
-#define print_real(pp)      print("\t%.15lf", pp)
-#define print_boolean(pp)   if (pp==1) print("\ttrue"); else print("\tfalse")
+#define  print        printf
+#define fprint       fprintf
+#define pprint       fprintf
+#define sprint       sprintf
+#define  println()    print("\n")
+#define fprintln(f)  fprint(f, "\n")
+#define pprintln(p)  pprint(p, "\n")
+#define sprintln(s)  sprint(s, "\n")
+#define  printab()    print("\t")
+#define fprintab(f)  fprint(f, "\t")
+#define pprintab(p)  pprint(p, "\t")
+#define sprintab(s)  sprint(s, "\t")
+#define print_integer(pi)   print("\t%ld", pi)
+#define print_real(pr)      print("\t%.15lf", pr)
+#define print_boolean(pb)   if (EQ(pb,1)) print("\ttrue"); else print("\tfalse")
 #define DebugWrong(s)           { print("\n[LINE %d] [FILE %s] [WRONG]: %s\n",__LINE__,__FILE__, s);}
 #define DebugCheck(c, s)        { if (c) print("\n[LINE %d] [FILE %s] [CHECK]: %s\n",__LINE__,__FILE__, s);}
 #define DebugW_T_F(s)           { print("\n[LINE %d] [FILE %s] [W-T-F]: %s\n",__LINE__,__FILE__, s);}
 #define DebugPrint(s)           { print("\n[LINE %d] [FILE %s] [Print]: %s\n",__LINE__,__FILE__, s);}
 #define DebugError(s, t)        { print("\n[LINE %d] [FILE %s] [ERROR]: %s\nreturning...\n",__LINE__,__FILE__, s); return t;}
-#define Debug0_Div(divisor)     { if (divisor==0) print("\n[LINE %d] [FILE %s] [0_Div]: ATTEMPT TO USE 0 AS DENOMINATOR!\n",__LINE__,__FILE__);}
-#define RandomSeed() srand((int)time(0))
-#define string_to_integer(s,t)  t=strtol(s, NULL, 10)  // string_to_integer(string, integer);
+#define Debug0_Div(divisor)     { if (EQ(divisor,0)) \
+                                  print("\n[LINE %d] [FILE %s] [0_Div]: ATTEMPT TO USE 0 AS DENOMINATOR!\n",__LINE__,__FILE__);}
+#define RandomSeed(s)            { if (s==0) srand(time(NULL)); else srand((int)s);}
+#define string_to_integer(s,t)  t=strtol(s, NULL, 10)  // use this macro in this way:   string_to_integer(string, integer);
 #define string_to_real(s,t)     t=strtod(s, NULL, 10)
-#define integer_to_string(s, t) sprint(t, "%ld", s) //integer_to_string(integer, string); Reserve the memory for string first!
+#define integer_to_string(s, t) sprint(t, "%ld", s) // integer_to_string(integer, string); Reserve the memory for string first!
 #define real_to_string(s, t)    sprint(t, "%lf", s)
-#define StartFromXOS  int main(int argc, char *argv[]){ if (!initialization(argc, argv)) DebugError("program cannot be initializated!", false);
-#define EndWithHonor  return deconstruction(); }
+#define BALL(x)                  typedef struct {x} // functions are threads, structs are balls ;-)
+#define StartFromXOS   int main(int argc, char *argv[]) { if (!initialization(argc, argv)) DebugError("program cannot be initializated!", false);
+#define EndWithHonor   return deconstruction(); }
 #define array_size(a)  (sizeof((a)) / sizeof((a[0])))
-#define iSwap(x,y) {integer tmp; tmp=x; x=y; y=tmp;}
-#define rSwap(x,y) {real    tmp; tmp=x; x=y; y=tmp;}
+#define iSwap(x,y)     { integer tmp; tmp=x; x=y; y=tmp; }
+#define rSwap(x,y)     { real    tmp; tmp=x; x=y; y=tmp; }
 //}}}
 
 boolean initialization(int user_command_number, char * user_commands[])/*{{{*/
