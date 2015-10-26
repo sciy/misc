c7755d73dfba29f216216dcef2bbc283a1b0c2c4
Mogei Wang <mogeiwang@gmail.com>
2015-10-26 14:22:24 +0800

    Update spectral-analysis.py

--- spectral-analysis.py
+++ spectral-analysis.py
@@ -1,24 +1,22 @@
 #  !/usr/bin/env python
 #  -*- coding:utf-8 -*-
-
 # http://www.cnblogs.com/stuarts/archive/2010/03/09/1681994.html
 
 from pylab import *
 import numpy as np
 
-nSampleNum = 5120
-ncount = 2048.0
-df = nSampleNum / ncount
-sampleTime = ncount / nSampleNum
-freqLine = 800
+sampleTime = 0.4 # x seconds totally
+ncount = 2048.0 # x sample points in total
+nSampleNum = ncount / sampleTime # how many samples per second
 
 x = np.linspace(0,sampleTime,ncount)
-sinx = np.sin(2*pi*250*x) + 0.5*np.sin(2*pi*500*x) + 0.3*np.sin(2*pi*1000*x)
+y = np.sin(2*pi*250*x) + 0.5*np.sin(2*pi*500*x) + 0.3*np.sin(2*pi*1000*x)
+freqLine = 800
+fft = np.fft.fft(y)[0:freqLine]
+fftx = np.linspace(0,freqLine/sampleTime,freqLine)
 
-fft = np.fft.fft(sinx)[0:freqLine]
-fftx = np.linspace(0,df*freqLine,freqLine)
 subplot(211)
-plot(x,sinx)
+plot(x,y)
 xlabel('time(s)')
 ylabel('amplitude')
 title('time domain graph')
