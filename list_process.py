888e4d11899312ac2e695eded83b8f1887fcc028
Mogei Wang <mogeiwang@gmail.com>
2015-10-26 13:15:05 +0800

    Create list_process.py

---
+++ list_process.py
@@ -0,0 +1,41 @@
+def outter_flatten(lst):
+    """only flat the outter level"""
+    new_lst = []
+    for x in lst:
+        if isinstance(x, list):
+            for y in x: new_lst.append(y)
+        else:
+            new_lst.append(x)
+    return new_lst
+
+# In [-]: a
+# Out[-]: [[[1, 2, 3], [1, 2, 3]], [[4, 5, 6], [4, 5, 6]], [[7, 8, 9], [7, 8, 9]]]
+# In [-]: shape(a)
+# Out[-]: (3, 2, 3)
+# In [-]: outter_flatten(a)
+# Out[-]: [[1, 2, 3], [1, 2, 3], [4, 5, 6], [4, 5, 6], [7, 8, 9], [7, 8, 9]]
+# In [-]: shape(outter_flatten(a))
+# Out[-]: (6, 3)
+# #-----------
+# In [-]: len(shape(a))
+# Out[-]: 3
+# In [-]: a=outter_flatten(a)
+# In [-]: len(shape(a))
+# Out[-]: 2
+# In [-]: a=outter_flatten(a)
+# In [-]: len(shape(a))
+# Out[-]: 1
+# In [-]: a
+# Out[-]: [1, 2, 3, 1, 2, 3, 4, 5, 6, 4, 5, 6, 7, 8, 9, 7, 8, 9]
+
+def equally_divide(lst, segment_len):
+    return [lst[i:i+segment_len] for i in range(0, len(lst), segment_len)]
+
+# In [-]: a
+# Out[-]: [1, 2, 3, 1, 2, 3, 4, 5, 6, 4, 5, 6, 7, 8, 9, 7, 8, 9]
+# In [-]: a=equally_divide(a,3)
+# Out[-]: [[1, 2, 3], [1, 2, 3], [4, 5, 6], [4, 5, 6], [7, 8, 9], [7, 8, 9]]
+# In [-]: a=equally_divide(a,2)
+# Out[-]: [[[1, 2, 3], [1, 2, 3]], [[4, 5, 6], [4, 5, 6]], [[7, 8, 9], [7, 8, 9]]]
+# In [-]: shape(a)
+# Out[-]: (3, 2, 3)
