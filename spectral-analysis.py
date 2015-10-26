db9eb769c66e9d775776a11060333e44d7430fff
Mogei Wang <mogeiwang@gmail.com>
2015-10-26 21:23:12 +0800

    Update spectral-analysis.py

--- spectral-analysis.py
+++ spectral-analysis.py
@@ -1,18 +1,19 @@
 #  !/usr/bin/env python
 #  -*- coding:utf-8 -*-
+
 # http://www.cnblogs.com/stuarts/archive/2010/03/09/1681994.html
 
 from pylab import *
 import numpy as np
 
 sampleTime = 0.4 # x seconds totally
-ncount = 2048.0 # x sample points in total
+sampleCount = 2048.0 # x sample points in total
 
-x = np.linspace(0,sampleTime,ncount)
+x = np.linspace(0, sampleTime, sampleCount)
 y = np.sin(2*pi*250*x) + 0.5*np.sin(2*pi*500*x) + 0.3*np.sin(2*pi*1000*x)
 freqLine = 800
-fft = np.fft.fft(y)[0:freqLine]
-fftx = np.linspace(0,freqLine/sampleTime,freqLine)
+ffty = np.fft.fft(y)[0:freqLine] # rfft can be used to calculate the fft since the data is real values
+fftx = np.linspace(0, freqLine/sampleTime, freqLine)
 
 subplot(211)
 plot(x,y)
@@ -21,8 +22,8 @@ ylabel('amplitude')
 title('time domain graph')
 
 subplot(212)
-plot(fftx,abs(fft))
-xlabel('freqency(Hz)')
+plot(fftx, abs(ffty)) # OR abs(ffty)**2 for power spectrum
+xlabel('frequency(Hz)')
 ylabel('amplitude')
 title('frequency domain graph')
 
